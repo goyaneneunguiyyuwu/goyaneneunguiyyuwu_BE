49378a55c264bbccb98aba5e139e475bd53d2dab
ohbin-kwon <kobbb2100@gmail.com>
2022-09-21 09:34:58 +0900

    feat: set pet entity / create, update dto - 논의(이슈: postgres query decimal type converted 'string' type issue #2)   decimal 또는 bigint type은 javascript의 number type에 할당되기에 큰 값이기 때문에 query를 하면 string값을 반환한다.   https://stackoverflow.com/questions/42901913/decimal-value-in-postgresql-returned-as-string-in-node-js   https://github.com/brianc/node-postgres/issues/811

--- src/pets/dto/update-pet.dto.ts
+++ src/pets/dto/update-pet.dto.ts
@@ -1,4 +1,95 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePetDto } from './create-pet.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+} from 'class-validator';
 
-export class UpdatePetDto extends PartialType(CreatePetDto) {}
+export class UpdatePetDto {
+  @IsNotEmpty()
+  @ApiProperty({
+    type: 'string',
+    example: '바둑이',
+    description: '반려동물의 이름',
+  })
+  @IsString()
+  @MaxLength(20)
+  name: string;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    type: 'number',
+    example: '3',
+    description: '반려동물의 종',
+  })
+  @Min(1)
+  @Max(2)
+  @IsNumber()
+  species: number;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    type: 'number',
+    example: '3',
+    description: '반려동물의 품종',
+  })
+  @Min(1)
+  @Max(155)
+  @IsNumber()
+  breeds: number;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    type: 'number',
+    example: '1',
+    description: '반려동물의 성별, 1: male, 2: female',
+  })
+  @Min(1)
+  @Max(2)
+  @IsNumber()
+  sex: number;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    type: 'date',
+    example: '2018-05-23T18:25:43.511Z',
+    description: '반려동물의 생일',
+  })
+  @IsDateString()
+  birthDate: Date;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    type: 'date',
+    example: '2018-05-23T18:25:43.511Z',
+    description: '반려동물과 함께 살기 시작한 날',
+  })
+  @IsDateString()
+  togetherDate: Date;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    type: 'decimal',
+    example: '3.5',
+    description: '반려동물의 무게',
+  })
+  @IsNumber(
+    { maxDecimalPlaces: 1 },
+    { message: 'weight must be a number conforming to the 1 decimal places' },
+  )
+  weight: string;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    type: 'boolean',
+    example: 'true',
+    description: '반려동물의 중성화 여부',
+  })
+  @IsBoolean()
+  neutering: boolean;
+}
--- src/pets/entities/pet.entity.ts
+++ src/pets/entities/pet.entity.ts
@@ -1,3 +1,4 @@
+import { Max, Min } from 'class-validator';
 import { Family } from 'src/users/entities/family.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Sex } from '../../types';
@@ -7,20 +8,40 @@ export class Pet {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  species: string;
-
-  @Column()
+  @Column({ type: 'varchar', length: 20 })
   name: string;
 
-  @Column()
-  birth: Date;
+  @Column({ type: 'int' })
+  @Min(1)
+  @Max(2)
+  species: number;
 
-  @Column()
-  together: Date;
+  @Column({ type: 'int' })
+  @Min(1)
+  @Max(155)
+  breeds: number;
 
-  @Column()
+  @Column({ type: 'int', enum: Sex })
+  @Min(1)
+  @Max(2)
   sex: Sex;
+
+  @Column()
+  birthDate: Date;
+
+  @Column()
+  togetherDate: Date;
+  // decimal 또는 bigint type은 javascript의 number type에 할당되기에 큰 값이기 때문에 string으로 저장이된다.
+  // 따라서 validation 시에는 number로 받지만, javascript 내부에서는 string으로 취급된다.
+  // https://stackoverflow.com/questions/42901913/decimal-value-in-postgresql-returned-as-string-in-node-js
+  // 아니 근데 이상한게 postgres에 저장되기 전까지는 number가 확실한데,,,?
+  // https://github.com/brianc/node-postgres/issues/811
+  // 여기에 좀더 명확한 설명이 나와있다. 저장되기전에는 number가 맞고, query로 불러온다면 string이 맞다. 내일 이것에 대해 이야기해 봐야겠다.
+  @Column({ type: 'decimal', precision: 3, scale: 1 })
+  weight: string;
+
+  @Column()
+  neutering: boolean;
 
   @ManyToOne(() => Family, family => family.pets)
   family: Family;
